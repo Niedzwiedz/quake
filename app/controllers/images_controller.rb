1ac80d931be33ce42daaa7a384ba77e8c11c63c2
Michael Nov <m93.autumn@gmail.com>
2016-03-12 13:14:01 +0100

    style, ajax form and index, show, new actions for image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,12 @@
 class ImagesController < ApplicationController
+  respond_to :html, :js
   def index
-    @images = Image.all
+    @images = Image.paginate(:page => params[:page], :per_page => 2)
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
   def create
     @image = Image.new(image_params)
@@ -11,8 +17,12 @@ class ImagesController < ApplicationController
     @image = Image.new
   end
 
+  def show
+    @image = Image.find(params[:id])
+  end
+
   private
     def image_params
-      params.require(:image).permit(:title, :file, :remote_file_url)
+      params.require(:image).permit(:id, :title, :file, :remote_file_url)
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   mount_uploader :file, FileUploader
+  validates :file, presence: true
+  validates :title, presence: true
 end
